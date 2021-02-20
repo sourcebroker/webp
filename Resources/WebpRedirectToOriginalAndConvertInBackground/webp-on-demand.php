82671d69a40396391ff52c5660cab21749f2d9a6
Krystian Szymukowicz <k.szymukowicz@gmail.com>
2021-02-20 15:49:08 +0100

    Fix missing namespace in webp-on-demand.php example.

--- Resources/WebpRedirectToOriginalAndConvertInBackground/webp-on-demand.php
+++ Resources/WebpRedirectToOriginalAndConvertInBackground/webp-on-demand.php
@@ -1,9 +1,9 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-
-(new WebpRedirectToOriginalAndConvertInBackground(
+(new \SourceBroker\Webp\WebpRedirectToOriginalAndConvertInBackground(
     [
+        // If you change this folder you need to change also htaccess part.
         'folderInDocumentRootToSaveWebp' => '/__processed__/webp-images',
     ]
 ))->start();
